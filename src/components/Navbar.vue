1f4dc8679949cb891c3f6e19a2476fcb701fc6a8
d3vkk <donaldkk22@yahoo.com>
2023-01-28 09:53:30 +0300

    refactor: Add Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex flex-row justify-between items-center">
+    <div class="rounded-full shadow-md p-2 bg-white">
+      <span v-html="iconArrowBack"></span>
+    </div>
+    <div
+      class="btn-primary px-2 py-2 bg-red-100 text-red-700 shadow-md rounded-md"
+    >
+      Upload Statement
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { iconArrowBack } from "../utils/icons";
+
+export default defineComponent({
+  setup() {
+    return {
+      iconArrowBack,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.btn-primary {
+  color: var(--white);
+  background-color: var(--secondary);
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="w-1/6">
-    <Sidebar />
-  </div>
+    <div>
+      <div class="w-1/6">
+        <Sidebar />
+      </div>
+      <div class="w-5/6">
+        <Navbar />
+      </div>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Navbar from "../components/Navbar.vue";
 import Sidebar from "../components/Sidebar.vue";
 
 export default defineComponent({
   components: {
+    Navbar,
     Sidebar,
   },
 });
