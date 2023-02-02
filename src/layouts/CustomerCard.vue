01b76031af3f5fe24896798500e364d29322b601
d3vkk <donaldkk22@yahoo.com>
2023-02-02 14:32:48 +0300

    refactor(a11y): Replace lists with sections

--- src/layouts/CustomerCard.vue
+++ src/layouts/CustomerCard.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="bg-white shadow rounded-md">
     <div class="card-title font-bold px-6 py-4">{{ cardTitle }}</div>
-    <ul class="card-body px-6 pt-2 pb-12">
-      <li
+    <section class="card-body px-6 pt-2 pb-12">
+      <article
         class="py-3 flex flex-row justify-between"
         v-for="body in cardList"
         :key="body.title"
       >
         <div class="card-details-title">{{ body.title }}</div>
         <div class="font-bold">{{ body.body }}</div>
-      </li>
+      </article>
       <Rangebar v-if="rangeList" :maxNum="rangeList!.max" :minNum="rangeList!.min" />
-    </ul>
+    </section>
   </div>
 </template>
 
